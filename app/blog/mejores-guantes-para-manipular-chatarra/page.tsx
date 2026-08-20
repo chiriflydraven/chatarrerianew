3a3f23987d597648183c937946d091a536d34841
Sergio Ruiz Merino <sergio.ruiz@rb-soluciones.com>
2026-08-20 23:45:34 +0200

    fix: wire explicit affiliate article route params

--- app/blog/mejores-guantes-para-manipular-chatarra/page.tsx
+++ app/blog/mejores-guantes-para-manipular-chatarra/page.tsx
@@ -1,1 +1,11 @@
-export { default, generateMetadata } from '../[slug]/page'
+import BlogPostPage, { generateMetadata as dynamicGenerateMetadata } from '../[slug]/page'
+
+const params = { slug: 'mejores-guantes-para-manipular-chatarra' }
+
+export function generateMetadata() {
+  return dynamicGenerateMetadata({ params })
+}
+
+export default function Page() {
+  return <BlogPostPage params={params} />
+}
